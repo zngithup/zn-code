811b2dc32f3f46aa27c0b605c4577af98e8bf14b
e <348959636@qq.com>
2020-01-01 14:46:59 +0800

    第一次写代码

---
+++ src/main/java/com/test/Demo.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @Descripption TODO
+ * @Author:zn
+ * @CreateDate:2020/1/1/001 14:46
+ * @UpdateDate:2020/1/1/001 14:46
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("第一次写代码。。。。。。。。");
+    }
+}
